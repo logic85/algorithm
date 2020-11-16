594de380a31bfd3421888908a8736ff4edb40030
logic85 <logic85@hotmail.com>
2020-11-16 15:50:02 +0900

    Create ExtendedEuclide.cpp

---
+++ ExtendedEuclide.cpp
@@ -0,0 +1,14 @@
+int x, y;
+void extendedEuclide(int r0, int r1, int s0, int s1, int t0, int t1) {
+    int r2 = r0 % r1;
+    if(r2 == 0) {
+        x = s1;
+        y = t1;
+        return;
+    }
+    
+    int q1 = r0 / r1;
+    int s2 = s0 - s1 * q1;
+    int t2 = t0 - t1 * q1;
+    extendedEuclide(r1, r2, s1, s2, t1, t2);
+}
